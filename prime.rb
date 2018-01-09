30826b6c037ec9cac9b19413ce9d107c2307003b
Larissa S <larissafschiavo@gmail.com>
2018-01-09 04:50:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  lesser_nums = (0...num).to_a
+  primes[0] = primes[1] = nil
+  p lesser_nums
+end
+
+def sieve_eratosthenes(biggest_num)
+  primes = (0..max).to_a
+  primes[0] = primes[1] = nil
+end
